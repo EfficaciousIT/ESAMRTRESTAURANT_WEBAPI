91fd44cf8915b7c266ef318103320cf69439b80c
EfficaciousIT <EfficaciousIT@gmail.com>
2021-10-16 17:33:21 +0530

    [ES-20] [WEB] Display Available table status to the waiter and manager

---
+++ Resto_WebApi/Controllers/TableStatusController.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Data;
+using Resto_WebApi.Models;
+using System.Web.Http;
+
+namespace Resto_WebApi.Controllers
+{
+    public class TableStatusController : ApiController
+    {
+        Database.DB record = new Database.DB();
+        public DataSet Get(string Command, int Res_Id)
+        {
+            TableStatus tableStatus = new TableStatus();
+            tableStatus.Res_Id = Res_Id;
+            DataSet ds = record.TableStatusDetails(Command, tableStatus);
+            return ds;
+        }
+    }
+}
---
+++ Resto_WebApi/Models/TableStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Resto_WebApi.Models
+{
+    public class TableStatus
+    {
+        public int Table_Id { get; set; }
+        public string Table_Status { get; set; }
+        public string Table_Name { get; set; }
+        public int Res_Id { get; set; }
+    }
+}
