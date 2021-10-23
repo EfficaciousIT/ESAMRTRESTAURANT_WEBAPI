d6965a7e058edb3a8feb9d547211ce5222912ab0
EfficaciousIT <EfficaciousIT@gmail.com>
2021-10-23 16:13:40 +0530

    [ES-17] [WEB] Create api for to take order from Waiter

---
+++ Resto_WebApi/Controllers/OrderDetailsController.cs
@@ -0,0 +1,29 @@
+﻿using Resto_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Resto_WebApi.Controllers
+{
+    public class OrderDetailsController : ApiController
+    {
+        Database.DB record = new Database.DB();
+        public HttpResponseMessage Post(string command, OrderDetails orderDetails)
+        {
+            try
+            {
+                DataSet ds = record.OrderDetails(command, orderDetails);
+                var message = Request.CreateResponse(HttpStatusCode.Created);
+                return message;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
---
+++ Resto_WebApi/Models/OrderDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Resto_WebApi.Models
+{
+    public class OrderDetails
+    {
+        public string OrderId { get; set; }
+        public int CategoryName { get; set; }
+        public int MenuName { get; set; }
+        public string TableName { get; set; }
+        public int RegisterId { get; set; }
+        public int EmployeeId { get; set; }
+        public int Price { get; set; }
+        public string Qty { get; set; }
+        public string vchFoodDescription { get; set; }
+        public string Kitchen_status { get; set; }
+        public string status { get; set; }
+    }
+}
